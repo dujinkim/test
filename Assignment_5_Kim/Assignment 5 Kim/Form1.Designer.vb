be63dbbb5caf2637427d13a88e33bbcbb19a4f30
dujinkim <juvyzz2337@gmail.com>
2019-06-26 19:34:21 -0400

    2019-06-26-2

---
+++ Assignment_5_Kim/Assignment 5 Kim/Form1.Designer.vb
@@ -0,0 +1,219 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.tbxID = New System.Windows.Forms.TextBox()
+        Me.tbxLastName = New System.Windows.Forms.TextBox()
+        Me.tbxFirstName = New System.Windows.Forms.TextBox()
+        Me.tbxExtension = New System.Windows.Forms.TextBox()
+        Me.tbxDepartment = New System.Windows.Forms.TextBox()
+        Me.btnRetreive = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnCreate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.lsvEmpList = New System.Windows.Forms.ListView()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(198, 265)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(21, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "ID:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(198, 294)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Last Name:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(198, 326)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "First Name:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(198, 353)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(56, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Extension:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(198, 381)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(65, 13)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Department:"
+        '
+        'tbxID
+        '
+        Me.tbxID.Location = New System.Drawing.Point(256, 258)
+        Me.tbxID.Name = "tbxID"
+        Me.tbxID.Size = New System.Drawing.Size(100, 20)
+        Me.tbxID.TabIndex = 6
+        '
+        'tbxLastName
+        '
+        Me.tbxLastName.Location = New System.Drawing.Point(256, 287)
+        Me.tbxLastName.Name = "tbxLastName"
+        Me.tbxLastName.Size = New System.Drawing.Size(100, 20)
+        Me.tbxLastName.TabIndex = 7
+        '
+        'tbxFirstName
+        '
+        Me.tbxFirstName.Location = New System.Drawing.Point(256, 319)
+        Me.tbxFirstName.Name = "tbxFirstName"
+        Me.tbxFirstName.Size = New System.Drawing.Size(100, 20)
+        Me.tbxFirstName.TabIndex = 8
+        '
+        'tbxExtension
+        '
+        Me.tbxExtension.Location = New System.Drawing.Point(256, 346)
+        Me.tbxExtension.Name = "tbxExtension"
+        Me.tbxExtension.Size = New System.Drawing.Size(100, 20)
+        Me.tbxExtension.TabIndex = 9
+        '
+        'tbxDepartment
+        '
+        Me.tbxDepartment.Location = New System.Drawing.Point(256, 374)
+        Me.tbxDepartment.Name = "tbxDepartment"
+        Me.tbxDepartment.Size = New System.Drawing.Size(100, 20)
+        Me.tbxDepartment.TabIndex = 10
+        '
+        'btnRetreive
+        '
+        Me.btnRetreive.Location = New System.Drawing.Point(37, 408)
+        Me.btnRetreive.Name = "btnRetreive"
+        Me.btnRetreive.Size = New System.Drawing.Size(75, 23)
+        Me.btnRetreive.TabIndex = 11
+        Me.btnRetreive.Text = "Retreive"
+        Me.btnRetreive.UseVisualStyleBackColor = True
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(357, 408)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(75, 23)
+        Me.btnUpdate.TabIndex = 12
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnCreate
+        '
+        Me.btnCreate.Location = New System.Drawing.Point(144, 408)
+        Me.btnCreate.Name = "btnCreate"
+        Me.btnCreate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCreate.TabIndex = 13
+        Me.btnCreate.Text = "Create"
+        Me.btnCreate.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(256, 408)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 23)
+        Me.btnDelete.TabIndex = 14
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(461, 408)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 15
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'lsvEmpList
+        '
+        Me.lsvEmpList.Location = New System.Drawing.Point(12, 12)
+        Me.lsvEmpList.Name = "lsvEmpList"
+        Me.lsvEmpList.Size = New System.Drawing.Size(552, 223)
+        Me.lsvEmpList.TabIndex = 16
+        Me.lsvEmpList.UseCompatibleStateImageBehavior = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(576, 450)
+        Me.Controls.Add(Me.lsvEmpList)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnCreate)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnRetreive)
+        Me.Controls.Add(Me.tbxDepartment)
+        Me.Controls.Add(Me.tbxExtension)
+        Me.Controls.Add(Me.tbxFirstName)
+        Me.Controls.Add(Me.tbxLastName)
+        Me.Controls.Add(Me.tbxID)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "PhoneBook"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents tbxID As TextBox
+    Friend WithEvents tbxLastName As TextBox
+    Friend WithEvents tbxFirstName As TextBox
+    Friend WithEvents tbxExtension As TextBox
+    Friend WithEvents tbxDepartment As TextBox
+    Friend WithEvents btnRetreive As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnCreate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents lsvEmpList As ListView
+End Class
---
+++ Assignment_5_Kim/Assignment 5 Kim/Form1.vb
@@ -0,0 +1,194 @@
+﻿Imports System.Data.SqlClient
+Public Class Form1
+    Dim strConnection As String = "Data Source=.\SQLEXPRESS;Initial Catalog=COMP10039A5;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        btnRetreive.Enabled = False
+        btnClear.Enabled = False
+        SetupEmployeesListView()
+        LoadEmployeesListView()
+
+
+
+        'Label1.Text = myCmd.Parameters("@name").Value
+
+    End Sub
+
+    Private Sub SetupEmployeesListView() 'Same as SetUpListview
+
+        lsvEmpList.View = View.Details
+        lsvEmpList.MultiSelect = False
+        lsvEmpList.GridLines = True
+        lsvEmpList.FullRowSelect = True
+
+        lsvEmpList.Columns.Add("ID", 50, HorizontalAlignment.Left)
+        lsvEmpList.Columns.Add("lastname", 100, HorizontalAlignment.Left)
+        lsvEmpList.Columns.Add("firstname", 100, HorizontalAlignment.Left)
+        lsvEmpList.Columns.Add("extension", 100, HorizontalAlignment.Left)
+        lsvEmpList.Columns.Add("department", 100, HorizontalAlignment.Left)
+
+    End Sub
+    Private Sub LoadEmployeesListView() 'Same as PopulateListview
+
+        Dim myConnection As SqlConnection = New SqlConnection(strConnection)
+
+        Try
+            myConnection.Open()
+            Dim myCmd As SqlCommand = New SqlCommand("Select Id, Lastname, Firstname, Extension, Department From Employees", myConnection)
+            Dim myDR As SqlDataReader = myCmd.ExecuteReader()
+            Dim intRow As Integer = 0
+            While myDR.Read()
+                intRow += 1
+                Dim lsiEmployees As ListViewItem = New ListViewItem(intRow)
+                'lsiEmployees.SubItems.Add(myDR("Id"))
+                lsiEmployees.SubItems.Add(myDR("Lastname"))
+                lsiEmployees.SubItems.Add(myDR("Firstname"))
+                lsiEmployees.SubItems.Add(myDR("Extension"))
+                lsiEmployees.SubItems.Add(myDR("Department"))
+                lsvEmpList.Items.Add(lsiEmployees)
+            End While
+            myDR.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.GetBaseException.ToString)
+        Finally
+            myConnection.Close()
+        End Try
+    End Sub
+
+
+    Private Sub lsvEmpList_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lsvEmpList.SelectedIndexChanged
+        btnRetreive.Enabled = True
+
+    End Sub
+
+    Private Sub btnCreate_Click(sender As Object, e As EventArgs) Handles btnCreate.Click
+        Dim myConnection As SqlConnection = New SqlConnection(strConnection)
+
+        Try
+            myConnection.Open()
+            Dim myCmd As SqlCommand = New SqlCommand()
+            myCmd.Connection = myConnection
+            myCmd.CommandText = "INSERT INTO Employees (Lastname, Firstname, Extension, Department)" +
+                "VALUES (@lastname, @firstname, @extension, @department)"
+
+            'myCmd.Parameters.Add("@id", SqlDbType.Int)
+            'myCmd.Parameters("@id").Value = Integer.Parse(tbxID.Text)
+            '@@@@@@@I needed to hide this statement because the instruction requires set the identify specification by 1@@@@@@@@@
+
+            myCmd.Parameters.Add("@lastname", SqlDbType.NChar)
+            myCmd.Parameters("@lastname").Value = tbxLastName.Text
+
+
+            myCmd.Parameters.Add("@firstname", SqlDbType.NChar)
+            myCmd.Parameters("@firstname").Value = tbxFirstName.Text
+
+
+
+            myCmd.Parameters.Add("@extension", SqlDbType.Int)
+            myCmd.Parameters("@extension").Value = Integer.Parse(tbxExtension.Text)
+
+
+            myCmd.Parameters.Add("@department", SqlDbType.NVarChar)
+            myCmd.Parameters("@department").Value = tbxDepartment.Text
+
+            Dim intRows As Integer = myCmd.ExecuteNonQuery()
+            If intRows = 1 Then
+                MessageBox.Show("Created")
+            Else
+                MessageBox.Show("Create Failed")
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            myConnection.Close()
+        End Try
+    End Sub
+
+    Private Sub btnRetreive_Click(sender As Object, e As EventArgs) Handles btnRetreive.Click
+        btnClear.Enabled = True
+        If lsvEmpList.SelectedItems.Count > 0 Then
+            Dim lviSelected As ListViewItem = lsvEmpList.SelectedItems(0)
+            tbxID.Text = lviSelected.SubItems(0).Text
+            tbxLastName.Text = lviSelected.SubItems(1).Text
+            tbxFirstName.Text = lviSelected.SubItems(2).Text
+            tbxExtension.Text = lviSelected.SubItems(3).Text
+            tbxDepartment.Text = lviSelected.SubItems(4).Text
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        tbxDepartment.Text = ""
+        tbxExtension.Text = ""
+        tbxFirstName.Text = ""
+        tbxID.Text = ""
+        tbxLastName.Text = ""
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Dim myConnection As SqlConnection = New SqlConnection(strConnection)
+
+        Try
+            myConnection.Open()
+            Dim myCmd As SqlCommand = New SqlCommand()
+            myCmd.Connection = myConnection
+            myCmd.CommandText = "DELETE FROM Employees WHERE Id = @Id"
+
+            myCmd.Parameters.Add("@Id", SqlDbType.Int)
+            myCmd.Parameters("@Id").Value = Integer.Parse(tbxID.Text)
+
+            Dim intRows As Integer = myCmd.ExecuteNonQuery()
+            If intRows = 1 Then
+                MessageBox.Show("Deleted")
+            Else
+                MessageBox.Show("Delete Failed")
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            myConnection.Close()
+        End Try
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Dim myConnection As SqlConnection = New SqlConnection(strConnection)
+
+        Try
+            myConnection.Open()
+            Dim myCmd As SqlCommand = New SqlCommand()
+            myCmd.Connection = myConnection
+            myCmd.CommandText = "UPDATE Employees SET Lastname = @lastname, Firstname = @firstname, Extension = @extension, Department = @department" +
+                "WHERE Id = @Id"
+
+            myCmd.Parameters.Add("@Id", SqlDbType.Int)
+            myCmd.Parameters("@Id").Value = Integer.Parse(tbxID.Text)
+
+            myCmd.Parameters.Add("@extension", SqlDbType.Int)
+            myCmd.Parameters("@extension").Value = Integer.Parse(tbxID.Text)
+
+            myCmd.Parameters.Add("@lastname", SqlDbType.NChar)
+            myCmd.Parameters("@lastname").Value = tbxLastName.Text
+
+            myCmd.Parameters.Add("@firstname", SqlDbType.NChar)
+            myCmd.Parameters("@firstname").Value = tbxLastName.Text
+
+            myCmd.Parameters.Add("@department", SqlDbType.NVarChar)
+            myCmd.Parameters("@department").Value = tbxLastName.Text
+
+
+            Dim intRows As Integer = myCmd.ExecuteNonQuery()
+            If intRows = 1 Then
+                MessageBox.Show("Updated")
+            Else
+                MessageBox.Show("Update Failed")
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            myConnection.Close()
+        End Try
+
+    End Sub
+End Class
